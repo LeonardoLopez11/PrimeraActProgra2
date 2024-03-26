4dc2d2dcfbe1d810c9031b9c5231ede2b3b79c40
LeonardoLopez11 <leonardolopeztolentino@gmail.com>
2024-03-26 12:22:46 -0500

    creación de Switch

--- Progra2PrimeraActividad/Program.cs
+++ Progra2PrimeraActividad/Program.cs
@@ -21,6 +21,34 @@ namespace Progra2PrimeraActividad
             {
                 Console.WriteLine("El número es negativo o cero.");
             }
+            
+            
+            Console.WriteLine("Ingrese un número del 1 al 5:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("Uno");
+                    break;
+                case 2:
+                    Console.WriteLine("Dos");
+                    break;
+                case 3:
+                    Console.WriteLine("Tres");
+                    break;
+                case 4:
+                    Console.WriteLine("Cuatro");
+                    break;
+                case 5:
+                    Console.WriteLine("Cinco");
+                    break;
+                default:
+                    Console.WriteLine("Ingresa otro número");
+                    break;
+            }
         }
+
     }
 }
